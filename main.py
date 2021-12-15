946bd977e012f3641bb662065b176b1b5ebb24e2
aaron <pencial@naver.com>
2021-12-15 23:00:38 +0900

    Add Html

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 from app.users import users
 from app.contents import contents
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="web/static"), name="static")
+templates = Jinja2Templates(directory="web/htmls")
 
 app.include_router(users.router)
 app.include_router(contents.router)
 
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def read_root(request: Request):
+    context = {'request': request}
+    return templates.TemplateResponse("main.html", context)
 
 
 if __name__ == "__main__":
